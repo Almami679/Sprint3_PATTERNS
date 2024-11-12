3694d6a76669b9dc02284e61a9f714772060dee3
Almami989 <almami989@gmail.com>
2024-11-12 13:32:30 +0100

    restructurando directorios

---
+++ 01_Patterns_1/src/main/java/Main/Main.java
@@ -0,0 +1,38 @@
+package Main;
+
+
+import Modules.Insert;
+import Modules.Undo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static final Undo SESSION = Undo.getInstance();
+    public static final String[] DEFAULTCOMMANDS = {"exit", "undo", "history"};
+    public static ArrayList<String> defaultCommands = new ArrayList<>(Arrays.asList(DEFAULTCOMMANDS));
+
+    public static void main(String[] args) {
+
+
+        boolean exit = false;
+
+        do {
+            System.out.println("> undo (go back to last command)\n" +
+                    "> history (see the command history)\n> exit (exit cmd)" +
+                    "\nWrite next command ... \n> ");
+
+            Insert input = Insert.getInstance();
+
+            String command = input.sendInput();
+
+            if(command.equalsIgnoreCase("exit")){
+                exit = true;
+            } else {
+                System.out.println(command + "\n");
+            }
+        } while(!exit);
+
+    }
+}
