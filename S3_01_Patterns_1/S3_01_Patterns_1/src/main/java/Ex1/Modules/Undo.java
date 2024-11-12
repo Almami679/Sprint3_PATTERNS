5956d6c6e1e25dec4e75d69c6888b83daebf5bdd
Almami989 <almami989@gmail.com>
2024-11-12 13:08:06 +0100

    Patterns_1/Nivel1/Ex1 completado

---
+++ S3_01_Patterns_1/S3_01_Patterns_1/src/main/java/Ex1/Modules/Undo.java
@@ -0,0 +1,40 @@
+package Ex1.Modules;
+
+import java.util.ArrayList;
+
+public class Undo {
+
+    private static Undo instance;
+    public static ArrayList<String> commands;
+
+
+    private Undo() {
+        commands = new ArrayList<>();
+
+    }
+
+    public synchronized static Undo getInstance() {
+        if (instance == null) {
+            instance = new Undo();
+        }
+        return instance;
+    }
+
+    public void saveStoric(String command) {
+        commands.add(command);
+    }
+
+    public void getStoric() {
+        commands.forEach(comm -> System.out.println("> " + comm));
+    }
+
+    public String undo() {
+        String command = commands.getLast();
+        commands.remove(commands.getLast());
+        return "last command used   > " + command;
+    }
+
+    public String executeCommand(String command) {
+        return "Comando " + command + " ejecutado";
+    }
+}
